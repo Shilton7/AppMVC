1b12b2ad498230d910e93fef5e7067df2f02fb4b
SHILTON PEREIRA SANTIAGO - 900000968 <shilton.santiago@animaeducacao.com.br>
2021-03-21 15:03:29 -0300

    Configuring new area route templates

--- src/DevIO.UI.Site/Modulos/Vendas/Controllers/PedidosController.cs
+++ src/DevIO.UI.Site/Modulos/Vendas/Controllers/PedidosController.cs
@@ -4,10 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DevIO.UI.Site.Areas.Produtos.Controllers
+namespace DevIO.UI.Site.Modulos.Vendas.Controllers
 {
-    [Area("Produtos")]
-    public class CadastroController : Controller
+    [Area("Vendas")]
+    public class PedidosController : Controller
     {
         public IActionResult Index()
         {
